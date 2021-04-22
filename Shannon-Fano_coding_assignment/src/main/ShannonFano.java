91c4963cbf30e8091e82975c715266e718b045c1
fekete20 <fekete20@iit.uni-miskolc.hu>
2021-04-22 23:33:24 +0200

    Fixed problems.

--- Shannon-Fano_coding_assignment/src/main/ShannonFano.java
+++ Shannon-Fano_coding_assignment/src/main/ShannonFano.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.function.Function;
@@ -17,9 +18,20 @@ public class ShannonFano {
 
 		ArrayList<Probabilities> elements = new ArrayList<>();
 		ArrayList<Double> probabilities = new ArrayList<>();
+		ArrayList<Double> order = new ArrayList<>();
+		Map<Character, Long> freqMap = null;
+		boolean isCharacter = false;
 		String path = "files" + File.separator;
 		Scanner scanner;
+		ArrayList<Character> characters = new ArrayList<>();
+
 		for (int i = 0; i < 5; i++) {
+			File resultFile = new File(System.getProperty("user.dir"), path + "result" + (i + 1) + ".txt");
+
+			Files.deleteIfExists(resultFile.toPath());
+			resultFile.createNewFile();
+			resultFile.setWritable(true, false);
+			FileWriter fileWriter = new FileWriter(resultFile);
 			System.out.println((i + 1) + ". fájl");
 			File sourceFile = new File(System.getProperty("user.dir"), path + "source" + (i + 1) + ".txt");
 			scanner = new Scanner(sourceFile);
@@ -28,17 +40,15 @@ public class ShannonFano {
 				if (isDouble(tmp)) {
 					probabilities.add(Double.parseDouble(tmp));
 				} else {
-					ArrayList<Character> characters = new ArrayList<>();
+					isCharacter = true;
 					for (int j = 0; j < tmp.length(); j++) {
 						characters.add(Character.toLowerCase(tmp.charAt(j)));
 					}
-					Map<Character, Long> freqMap = characters.stream()
+					freqMap = characters.stream()
 							.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 					for (Map.Entry<Character, Long> entry : freqMap.entrySet()) {
 						probabilities.add((double) entry.getValue() / characters.size());
 					}
-					characters.clear();
-					freqMap.clear();
 				}
 			}
 			scanner.close();
@@ -49,55 +59,72 @@ public class ShannonFano {
 			}
 
 			System.out.println((i + 1) + ". forrásnál " + "P halmaz:");
+			fileWriter.write("P halmaz elemei: \n");
 			for (Probabilities prob : elements) {
 				System.out.print(prob.getValue() + " ");
+				fileWriter.write(prob.getValue() + " ");
 			}
 
 			Collections.sort(elements, Collections.reverseOrder());
-
 			System.out.println("\n" + (i + 1) + ". forrásnál " + "P* halmaz:");
+			fileWriter.write("\nP* halmaz elemei: \n ");
 			for (Probabilities prob : elements) {
 				System.out.print(prob.getValue() + " ");
+				fileWriter.write(prob.getValue() + " ");
 			}
 
 			// X*
-			elements.get(0).setxStar(0.00); // fix
+			elements.get(0).setxStar(0.00);
 			for (int j = 1; j < elements.size(); j++) {
 				elements.get(j).setxStar(sum(elements, j - 1));
 			}
 
 			System.out.println("\n" + (i + 1) + ". forrásnál " + "X* halmaz: ");
+			fileWriter.write("\nX* halmaz elemei: \n ");
 			for (Probabilities prob : elements) {
 				System.out.print(prob.getxStar() + " ");
+				fileWriter.write(prob.getxStar() + " ");
 			}
 
 			// division and coding
 			codewords(elements, 0, 1, 0.5);
 
 			System.out.println("\n" + (i + 1) + ". forrásnál " + " kódszavak: ");
-
-			File resultFile = new File(System.getProperty("user.dir"), path + "result" + (i + 1) + ".txt");
-
-			Files.deleteIfExists(resultFile.toPath());
-			resultFile.createNewFile();
-			resultFile.setWritable(true, false);
-			FileWriter fileWriter = new FileWriter(resultFile);
-			for (Probabilities prob : elements) {
-				System.out.print(prob.getCodeword() + " ");
-				fileWriter.write(prob.getCodeword());
+			freqMap = freqMap.entrySet().stream().sorted((Map.Entry.<Character, Long>comparingByValue().reversed()))
+					.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1,
+							LinkedHashMap::new));
+			if (isCharacter) {
+				int j = 0;
+				for (Map.Entry<Character, Long> entry : freqMap.entrySet()) {
+					if (j == 0) {
+						System.out.println("Karakter\tDarabszám\tRelatív gyakoriság\tKódszó");
+						fileWriter.write("\nKarakter\tDarabszám\tRelatív gyakoriság\tKódszó\n");
+					}
+					System.out.print(entry.getKey());
+					fileWriter.write(entry.getKey());
+					System.out.print("\t\t" + entry.getValue());
+					fileWriter.write("\t\t\t" + entry.getValue());
+					System.out.print("\t\t" + elements.get(j).getValue());
+					fileWriter.write("\t\t\t" + elements.get(j).getValue());
+					System.out.println("\t" + elements.get(j).getCodeword());
+					fileWriter.write("\t\t" + elements.get(j).getCodeword() + "\n");
+					j++;
+				}
+				freqMap.clear();
 			}
 
-			System.out.println("\n" + (i + 1) + ". forrásnál " + "hatásfok: " + codingEfficiency(elements));
-			fileWriter.write("\nHatásfok: " + codingEfficiency(elements));
+			System.out.println((i + 1) + ". forrásnál " + "entrópia hatásfok: " + codingEfficiency(elements));
+			fileWriter.write("\nEntrópia hatásfok értékek: " + codingEfficiency(elements));
 			fileWriter.close();
 			probabilities.clear();
 			elements.clear();
+			characters.clear();
+			order.clear();
 			System.out.println();
 		}
 	}
 
 // auxiliary functions
-
 	int codewords(ArrayList<Probabilities> elements, double start, double end, double length) {
 		int multiplicityOfInterval = 0;
 		for (double i = start, intervalNo = 0; i < end && intervalNo < 2; i += length, intervalNo++) {
